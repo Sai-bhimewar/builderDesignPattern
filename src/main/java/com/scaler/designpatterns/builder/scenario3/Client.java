a9b183b463e1cd0f47e386480ffd8b90abd52beb
Saiprasad Balaji Bhimewar <saibhimewar788@gmail.com>
2025-03-18 19:26:37 +0530

    Added some changes

--- src/main/java/com/scaler/designpatterns/builder/scenario3/Client.java
+++ src/main/java/com/scaler/designpatterns/builder/scenario3/Client.java
@@ -4,14 +4,14 @@ package com.scaler.designpatterns.builder.scenario3;
 public class Client {
     public static void main(String[] args) {
         Student st = Student.getBuilder()
-                .setId("1").
-                setAge(20).
-                setName("Sai").
-                setPsp(90.3).
-                setUniversity("Pune").
-                setPhone("1234567890").
-                setGradeYear(2023).
-                build();
+                .setId("1")
+                .setAge(20)
+                .setName("Sai")
+                .setPsp(90.3)
+                .setUniversity("Pune")
+                .setPhone("1234567890")
+                .setGradeYear(2023)
+                .build();
         System.out.println(st);
 
 
